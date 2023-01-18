bf5785d14adb3a06dc13bad8658b78032399c591
Don Macary <don.macary@gmail.com>
2023-01-18 09:30:13 -0800

    Modifed MessageBoxW to utilize D/Invoke

--- MessageBox/MessageBox/Program.cs
+++ MessageBox/MessageBox/Program.cs
@@ -5,16 +5,23 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Runtime.InteropServices;
 
+using DInvoke.DynamicInvoke;
+
 namespace MessageBox
 {
     internal class Program
     {
-        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
-        static extern int MessageBoxW(IntPtr hWnd, string lpText, string lpCaption, uint uType);
+        [UnmanagedFunctionPointer(CallingConvention.StdCall, CharSet = CharSet.Unicode)]
+        delegate int MessageBoxW(IntPtr hWnd, string lpText, string pCaption, uint uType);
+
 
         static void Main(string[] args)
         {
-            MessageBoxW(IntPtr.Zero, "My first P/Invoke", "Hello World", 0);
+            var address = Generic.GetLibraryAddress("user32.dll", 2155);
+            var totallylegitAPI = (MessageBoxW)Marshal.GetDelegateForFunctionPointer(address, typeof(MessageBoxW));
+
+            totallylegitAPI(IntPtr.Zero, "Box 1", "Box 1", 0);
+            totallylegitAPI(IntPtr.Zero, "Box 2", "Box 2", 0);
         }
     }
 }
