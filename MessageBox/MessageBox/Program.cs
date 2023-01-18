ba45ca7bec0b778f2c005e2635a2f75a3cdbbc0e
Don Macary <don.macary@gmail.com>
2023-01-18 08:22:08 -0800

    Added message box

---
+++ MessageBox/MessageBox/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace MessageBox
+{
+    internal class Program
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        static extern int MessageBoxW(IntPtr hWnd, string lpText, string lpCaption, uint uType);
+
+        static void Main(string[] args)
+        {
+            MessageBoxW(IntPtr.Zero, "My first P/Invoke", "Hello World", 0);
+        }
+    }
+}
